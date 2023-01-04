2f6859f7620c63a0fdb950e755372da471317e95
tugay aras <kes.ici@hotmail.com>
2023-01-04 11:01:13 +0300

    şema için tüm componentler belirlendi ve tüm kullanılacak resimler eklendi

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css';
+import {GiHamburgerMenu} from 'react-icons/gi';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Newsletter from './Footer/Newsletter';
+import FooterOverlay from './Footer/FooterOverlay';
+import MenuItem from './MenuItem/MenuItem';
+import Navbar from './Navbar/Navbar';
+import SubHeading from './SubHeading/SubHeading';
+
+export{
+    SubHeading,
+    Newsletter,
+    MenuItem,
+    Navbar,
+    FooterOverlay
+}
